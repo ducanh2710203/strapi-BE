ac349b6f50714c5a667061e25265e5b7d4cb4a34
ducanh2710203 <duc2710203@gmail.com>
2024-10-06 02:04:30 +0700

    mongo onl

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
-// config/database.js
-
-module.exports = ({ env }) => ({
+module.exports = () => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI", "mongodb://localhost:27017/demotttn"), // Sử dụng 'DATABASE_URI' trong tệp .env
+        uri: "mongodb+srv://duc2710203:ducanh2710@cluster0.lyvby.mongodb.net/tttn?retryWrites=true&w=majority&appName=Cluster0",
       },
       options: {
-        ssl: false,
+        ssl: true, // Sử dụng true cho MongoDB Atlas (SSL kết nối an toàn)
       },
     },
   },
